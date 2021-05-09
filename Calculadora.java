71314255ee7d0920db517f15b2f49fc635c8dff4
sergiosob <sobrito@gmail.com>
2021-05-08 21:02:04 -0300

    Calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -5,118 +5,153 @@ import java.util.Scanner;
 
 public class Calculadora {
 	
-		public int som(int num1, int num2) {
-	  
-		return num1 + num2;
-		
-	}
-	public int sub(int num1, int num2){
-	   
-		return num1 - num2;
-		
-	}
-	public int div(int num1,int num2){
-	    
-		return num1 / num2;
-		
-	}
-	public int mult(int num1, int num2){
-	     
-		return num1 * num2;
-		
-	}
-	
-	
-	
 	public static void main (String args[]){
 		
+		int opcao = 9;
+		double operacao;
+		double num1;
+		double num2;
+	
 		
-		Calculadora c = new Calculadora();
-		
-		
-		int opcao = 5;
-		int num1;
-		int num2;
-		
-	      Scanner input = new Scanner(System.in);  
-	      
-	      
-		System.out.println("-Escolha uma opção-");
-	    System.out.println("1. Soma");  
-	    System.out.println("2. Subtracao");  
-	    System.out.println("3. Multiplicacao");  
-	    System.out.println("4. Divisao");  
-	    System.out.println("0. Sair");  
-	    System.out.print("Operação: ");  
-	    
-	    opcao = input.nextInt();
-	    input.close();
+		Scanner entrada = new Scanner(System.in);  
 	    
 	    while (opcao != 0) {
+	    	if (opcao >= 1 && opcao <= 8) {
+	    	switch (opcao) {
 	    	
-	    
-	    	if (opcao == 1) {
+	    	case 1:
+	    	  
+	    		System.out.print("Qual o primeiro numero?: ");
+	    	    
+	    		num1 = entrada.nextDouble();
 	    		
+	    		System.out.print("Qual o segundo numero?: ");
 	    		
-	    	    Scanner input1 = new Scanner(System.in);  
-	  	      
-	    		System.out.print("Qual o primeiro numero: ");
-	    		 num1 = input1.nextInt();
-	    		System.out.print("Qual o segundo numero: ");
-	    		 num2 = input1.nextInt();
-	    		int operacao = c.som(num1, num2);
+	    		num2 = entrada.nextDouble();
+	    		
+	    		operacao = num1 + num2;
 	    		System.out.println("Resultado");
 	    		System.out.println(operacao);
-	    		input1.close();
-	    		break;
-	    	}
-	    	 	if (opcao == 2) {
-		    		
-	    	 		Scanner input1 = new Scanner(System.in);  
-	    	  	      
-	  	    		System.out.print("Qual o primeiro numero: ");
-	  	    		 num1 = input1.nextInt();
-	  	    		System.out.print("Qual o segundo numero: ");
-	  	    		 num2 = input1.nextInt();
-	  	    		int operacao = c.sub(num1, num2);
+	    		opcao = 0;
+				break;
+	    	
+	    	case 2: 
+	    		
+	    	 		
+	  	    		System.out.print("Qual o primeiro numero?: ");
+	  	    	   
+	  	    		num1 = entrada.nextDouble();
+	  	    		
+	  	    		System.out.print("Qual o segundo numero?: ");
+	  	    	  
+	  	    		num2 = entrada.nextDouble();
+	  	    		
+	  	    		operacao = num1 - num2;
 	  	    		System.out.println("Resultado");
 	  	    		System.out.println(operacao);
-	  	    		input1.close();
-		    		
+	  	    		opcao = 0;
 		    		break;
-		    		}
-		    	 	if (opcao == 3) {
-		    	 		  Scanner input1 = new Scanner(System.in);  
-		    	  	      
-		  	    		System.out.print("Qual o primeiro numero: ");
-		  	    		 num1 = input1.nextInt();
-		  	    		System.out.print("Qual o segundo numero: ");
-		  	    		num2 = input1.nextInt();
-		  	    		int operacao = c.mult(num1, num2);
+		    		
+	    	case 3:
+		    	 		   
+		  	    		System.out.print("Qual o primeiro numero?: ");
+		  	    		
+		  	    		num1 = entrada.nextDouble();
+		  	    		
+		  	    		System.out.print("Qual o segundo numero?: ");
+		  	    		
+		  	    		num2 = entrada.nextDouble();
+		  	    		
+		  	    		operacao = num1 * num2;
 		  	    		System.out.println("Resultado");
 		  	    		System.out.println(operacao);
-		  	    		input1.close();
+		  	    		opcao = 0;
 			    		break;
-		    	 	}
-			    	 	if (opcao == 4) {
-			    	 		  Scanner input1 = new Scanner(System.in);  
-			    	  	      
-			  	    		System.out.print("Qual o primeiro numero: ");
-			  	    		 num1 = input1.nextInt();
-			  	    		System.out.print("Qual o segundo numero: ");
-			  	    		 num2 = input1.nextInt();
-			  	    		int operacao = c.div(num1, num2);
-			  	    		System.out.println("Resultado");
-			  	    		System.out.println(operacao);
-			  	    		input1.close();
-				    		break;
-				    		}
-			    	 	else{
-					    	System.out.print("????");
-					    	break;
-					    		}
-			    	 	
+		    	 	 
+	    	case 4:
+			    	 	  
+			  	    	System.out.print("Qual o primeiro numero?: ");
+			  	    	
+			  	    	num1 = entrada.nextDouble();
+			  	    	
+			  	    	System.out.print("Qual o segundo numero?: ");
+			  	    	
+			  	    	num2 = entrada.nextDouble();
+			  	    	
+			  	    	operacao = (num1 / num2);
+			  	    	System.out.println("Resultado");
+			  	    	System.out.println(operacao);
+			  	    	opcao = 0;
+			  	    	break;
+				     
+	    	case 5: 
+				  	     System.out.print("Qual o número?: ");
+				  	     
+				  	     num1 = entrada.nextDouble();
+				  	     
+				  	     operacao = Math.sqrt(num1);
+				  	     System.out.println("Resultado");
+				  	     System.out.println(operacao);
+				  	     opcao = 0;
+				  	     break;
+	    	case 6:  
+				  	     System.out.print("Qual o numero?: ");
+				  	     
+				  	     num1 = entrada.nextDouble();
+				  	     
+				  	     System.out.print("Qual o expoente?: ");
+				  	     
+				  	     num2 = entrada.nextDouble();
+				  	     
+				  	     operacao = Math.pow(num1, num2);
+				  	     System.out.println("Resultado");
+				  	     System.out.println(operacao);
+				  	     opcao = 0;
+					     break;
+	     	case 7:  
+	     				System.out.print("Qual o número?: ");
+	     				
+	     				num1 = entrada.nextDouble();
+	     				
+	     				if (num1 % 2 == 0) {
+							System.out.println(" É par.");
+						} else {
+							System.out.println(" É impar.");
+						}
+	     				opcao = 0;
+	     				break;
+	       	case 8:  
+	       				System.out.print("Qual o número?: ");
+	       				
+	       				num1 = entrada.nextDouble();
+	       				
+	       				if (num1 % 3 == 0) {
+	       				System.out.println("SIM é DIVISIVEL");
+	       				} else {
+	       				System.out.println("NÃO, não é divisivel");
+	       				}
+	       				opcao = 0;
+	       				break;
+	    	}
+	    	} else {
+
+				System.out.println("-Escolha uma opção-");
+			    System.out.println("1. Soma");  
+			    System.out.println("2. Subtracao");  
+			    System.out.println("3. Multiplicacao");  
+			    System.out.println("4. Divisao");  
+			    System.out.println("5. Raiz Quadrada");
+			    System.out.println("6. Exponenciação");
+			    System.out.println("7. Testa Par");
+			    System.out.println("8. Testa Divisível por 3");  
+			    System.out.println("0. Finaliza Calculadora");
+			    System.out.print("Operação: "); 
+				opcao = entrada.nextInt();
+			
+	    	}
+	    	}
+
+		System.out.println("Operação Finalizada");
+		entrada.close();
 	    }
 	}
-}
-
